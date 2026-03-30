4bbde9b58ade315a6bc1fc894fca344ff233d0f2
fvrlak <fvrlak@gmail.com>
2026-03-30 22:17:26 +0200

    Remove centered heading element

--- apps/vite-default/src/App.tsx
+++ apps/vite-default/src/App.tsx
@@ -1,15 +1,7 @@
 import './App.css'
 
 function App() {
-  return (
-    <main className="chat-page">
-      <section className="chat-shell" aria-label="Centered heading">
-        <div className="center-heading" role="heading" aria-level={1}>
-          /home/fvrlak/ventures/softbox/apps/vite-default
-        </div>
-      </section>
-    </main>
-  )
+  return <main className="chat-page" />
 }
 
 export default App
